c49b23956d086885d49328ba7bd2c8876545f08f
Tatsukane <tatsukaneakira2529@gmail.com>
2025-03-23 23:25:47 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,20 @@
+const signInButton = document.getElementById("signInBtn");
+const alert = document.querySelector("p");
+
+signInButton.addEventListener("click", () => {
+	const username = document.getElementById("username").value;
+	const password = document.getElementById("password").value;
+
+	if (username === "tphuong._.24" && password === "02042006") {
+		window.location.href = "C:\\Users\\Admin\\Pictures\\Code\\w\\fh\\About Us\\aboutUs.html";
+	}
+	else {
+		alert.classList.add("alert");
+		setTimeout(cl, 1500);
+		return;
+	}
+})
+
+function cl(){
+	alert.classList.remove("alert");
+}
